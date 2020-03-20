7a679c7651264f83826b1903d9b83fa19fb21c89
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-03-20 14:38:54 +0900

    :white_check_mark: Write some tests to LibNgxBrPipe

--- projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.spec.ts
+++ projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.spec.ts
@@ -6,3 +6,30 @@ describe('LibNgxBrPipe', () => {
     expect(pipe).toBeTruthy();
   });
 });
+
+describe('Convert all line breaks to br elements', () => {
+  it('Convert one line break to br element', () => {
+    const pipe = new LibNgxBrPipe();
+    expect(pipe.transform('foo\nbar')).toBe('foo<br>bar');
+  });
+
+  it('Convert three line breaks to br elements', () => {
+    const pipe = new LibNgxBrPipe();
+    expect(pipe.transform('foo\n\n\nbar')).toBe('foo<br><br><br>bar');
+  });
+
+  it('All line breaks convert to br elements in Each places', () => {
+    const pipe = new LibNgxBrPipe();
+    expect(pipe.transform('foo\nbar\n\nbaz')).toBe('foo<br>bar<br><br>baz');
+  });
+
+  it('Ignore converting line break on head of line', () => {
+    const pipe = new LibNgxBrPipe();
+    expect(pipe.transform('\nfoo\nbar\n\nbaz\nqux')).toBe('foo<br>bar<br><br>baz<br>qux');
+  });
+
+  it('Ignore converting line break on end of line', () => {
+    const pipe = new LibNgxBrPipe();
+    expect(pipe.transform('foo\nbar\n\nbaz\nqux\n\n')).toBe('foo<br>bar<br><br>baz<br>qux');
+  });
+});
--- projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.ts
+++ projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LibNgxBrPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]) {
-    return value.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    return value.trim().replace(/(?:\r\n|\r|\n)/g, '<br>');
   }
 
 }
