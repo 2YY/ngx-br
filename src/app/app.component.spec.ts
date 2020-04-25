82a13864ef01401457266f1c7251d2a7f91578cb
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-04-26 01:26:25 +0900

    :white_check_mark: fix app component unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {LibNgxBrModule} from '../../projects/lib-ngx-br/src/lib/lib-ngx-br.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        LibNgxBrModule
       ],
       declarations: [
         AppComponent
