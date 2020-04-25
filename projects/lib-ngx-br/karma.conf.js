7bbd56ba7a794fe9b9e429d8a5978b5478ae67da
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-04-25 21:04:51 +0900

    :art: Remove duplicated Karma configs

--- projects/lib-ngx-br/karma.conf.js
+++ projects/lib-ngx-br/karma.conf.js
@@ -1,33 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const plugins = [
-  require('karma-jasmine'),
-  require('karma-chrome-launcher'),
-  require('karma-jasmine-html-reporter'),
-  require('karma-coverage-istanbul-reporter'),
-  require('@angular-devkit/build-angular/plugins/karma')
-];
+const commonKarmaConfigs = require('../../karma.conf.js');
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins,
-    client: { clearContext: false }, // leave Jasmine Spec Runner output visible in browser
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/lib-ngx-br'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    customLaunchers: { ChromeHeadlessCI: { base: 'ChromeHeadless', flags: ['--no-sandbox'] } },
-    singleRun: false,
-    restartOnFileChange: true
-  });
+  commonKarmaConfigs(config);
 };
