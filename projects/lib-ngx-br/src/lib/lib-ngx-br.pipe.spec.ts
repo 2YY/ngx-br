46eea825d176e383dfe324d439154e33f945c03f
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-04-25 20:56:04 +0900

    :art: Remove duplicated pipe instance describe

--- projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.spec.ts
+++ projects/lib-ngx-br/src/lib/lib-ngx-br.pipe.spec.ts
@@ -1,35 +1,29 @@
 import { LibNgxBrPipe } from './lib-ngx-br.pipe';
 
 describe('LibNgxBrPipe', () => {
+  const pipe = new LibNgxBrPipe();
+
   it('create an instance', () => {
-    const pipe = new LibNgxBrPipe();
     expect(pipe).toBeTruthy();
   });
-});
 
-describe('Convert all line breaks to br elements', () => {
-  it('Convert one line break to br element', () => {
-    const pipe = new LibNgxBrPipe();
+  it('convert one line break to br element', () => {
     expect(pipe.transform('foo\nbar')).toBe('foo<br>bar');
   });
 
-  it('Convert three line breaks to br elements', () => {
-    const pipe = new LibNgxBrPipe();
+  it('convert three line breaks to br elements', () => {
     expect(pipe.transform('foo\n\n\nbar')).toBe('foo<br><br><br>bar');
   });
 
-  it('All line breaks convert to br elements in Each places', () => {
-    const pipe = new LibNgxBrPipe();
+  it('all line breaks convert to br elements in Each places', () => {
     expect(pipe.transform('foo\nbar\n\nbaz')).toBe('foo<br>bar<br><br>baz');
   });
 
-  it('Ignore converting line break on head of line', () => {
-    const pipe = new LibNgxBrPipe();
+  it('ignore converting line break on head of line', () => {
     expect(pipe.transform('\nfoo\nbar\n\nbaz\nqux')).toBe('foo<br>bar<br><br>baz<br>qux');
   });
 
-  it('Ignore converting line break on end of line', () => {
-    const pipe = new LibNgxBrPipe();
+  it('ignore converting line break on end of line', () => {
     expect(pipe.transform('foo\nbar\n\nbaz\nqux\n\n')).toBe('foo<br>bar<br><br>baz<br>qux');
   });
 });
